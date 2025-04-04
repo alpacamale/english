c8957de41d1b34dde018fecc3f161732833ad761
AlpacaMale <alpaca9100@gmail.com>
2025-04-05 03:52:22 +0900

    fix: correct return type

--- functions.py
+++ functions.py
@@ -154,7 +154,7 @@ def get_video_ids() -> list[str]:
     return [video_path.lstrip("files/") for video_path in glob("files/*")]
 
 
-def get_video_name_map() -> list[str]:
+def get_video_name_map() -> dict:
     video_ids = get_video_ids()
     return {get_video_name(video_id): video_id for video_id in video_ids}
 
