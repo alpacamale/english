d1f960973316ac40f36931e97829a684b3b47b71
AlpacaMale <alpaca9100@gmail.com>
2025-04-05 18:26:49 +0900

    feat: error handle when no video, specify return type

--- pages/01_shadow.py
+++ pages/01_shadow.py
@@ -14,14 +14,14 @@ def load_text(video_id: str) -> list[dict]:
     return parser.parse(text)
 
 
-def select_video() -> tuple[str, str, str]:
+def select_video():
     """
     Return video metadata
 
     Returns:
         - video_id (str): The video id what user selected via name
         - video_dir (str): The directory path where the video is stored
-        - video_path (str): The full file path to the video
+        - video_path (str): The file path to the video
     """
     video_names = get_video_names()
     video_name_map = get_video_name_map()
@@ -61,34 +61,36 @@ if "initial" not in st.session_state:
         }
     )
 
-video_id, video_dir, video_path = select_video()
+if get_video_names() == []:
+    st.write("### You need to upload the video first!")
+else:
+    video_id, video_dir, video_path = select_video()
 
-st.title(title)
-st.video(
-    video_path,
-    start_time=st.session_state["start_time"],
-    end_time=st.session_state["end_time"] if st.session_state["loop"] else None,
-    loop=st.session_state["loop"],
-)
+    st.video(
+        video_path,
+        start_time=st.session_state["start_time"],
+        end_time=st.session_state["end_time"] if st.session_state["loop"] else None,
+        loop=st.session_state["loop"],
+    )
 
-repeat_column, caption_column, record_column = st.columns(3)
-with repeat_column:
-    repeat = st.toggle("repeat", st.session_state["loop"], key="repeat_toggle")
-with caption_column:
-    caption = st.toggle("caption", True, key="caption_toggle")
-with record_column:
-    record = st.toggle("record voice", False, key="record_toggle")
+    repeat_column, caption_column, record_column = st.columns(3)
+    with repeat_column:
+        repeat = st.toggle("repeat", st.session_state["loop"], key="repeat_toggle")
+    with caption_column:
+        caption = st.toggle("caption", True, key="caption_toggle")
+    with record_column:
+        record = st.toggle("record voice", False, key="record_toggle")
 
-if repeat != st.session_state["loop"]:
-    st.session_state["loop"] = repeat
-    st.rerun()
+    if repeat != st.session_state["loop"]:
+        st.session_state["loop"] = repeat
+        st.rerun()
 
-if caption:
-    with st.container(border=True, height=200):
-        captions = load_text(video_id)
-        for caption in captions:
-            button = st.button(caption["text"], key=caption["start"])
-            if button:
-                st.session_state["start_time"] = caption["start"]
-                st.session_state["end_time"] = caption["end"]
-                st.rerun()
+    if caption:
+        with st.container(border=True, height=200):
+            captions = load_text(video_id)
+            for caption in captions:
+                button = st.button(caption["text"], key=caption["start"])
+                if button:
+                    st.session_state["start_time"] = caption["start"]
+                    st.session_state["end_time"] = caption["end"]
+                    st.rerun()
