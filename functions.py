6ad422199441f510c500e34c03833e2922d7ce21
AlpacaMale <alpaca9100@gmail.com>
2025-04-07 11:00:56 +0900

    refac: organize code and change creation base directory structure

--- functions.py
+++ functions.py
@@ -20,59 +20,95 @@ def get_video_id(url: str) -> str:
     return url.lstrip("https://www.youtube.com/watch?v=")
 
 
-def get_video_dir(video_id: str) -> str:
+def get_base_dir(video_id: str) -> str:
     return f"files/{video_id}"
+
+
+def get_video_path(base_dir: str) -> str:
+    extensions = ["mp4", "avi", "mov", "webm"]
+    files = glob(f"{base_dir}/media/*.*")
+    return next((file for file in files if file.split(".")[-1] in extensions), None)
+
+
+def get_audio_path(base_dir: str) -> str:
+    return f"{base_dir}/media/audio.mp3"
+
+
+# def get_chunk_dir(base_dir: str) -> str:
+#     return f"{base_dir}/media/chunks"
+
+
+def get_audio_chunk_dir(base_dir: str) -> str:
+    return f"{base_dir}/media/chunks/audio"
+
+
+def get_echo_chunk_dir(base_dir: str) -> str:
+    return f"{base_dir}/media/chunks/echo"
+
+
+def get_transcript_path(base_dir: str) -> str:
+    return f"{base_dir}/transcript.txt"
+
+
+def get_metadata_path(base_dir: str) -> str:
+    return f"{base_dir}/meta.json"
+
+
+def get_video_name(base_dir: str) -> str:
+    metadata_path = get_metadata_path(base_dir)
+    with open(metadata_path, "r") as f:
+        result = json.loads(f.read())
+    return result["title"]
+
+
+def get_video_ids() -> list[str]:
+    return [video_path.lstrip("files/") for video_path in glob("files/*")]
+
+
+def get_video_name_map(video_ids: list[str]) -> dict:
+    return {get_video_name(video_id): video_id for video_id in video_ids}
+
+
+def get_video_names(video_ids: list[str]) -> list[str]:
+    return [get_video_name(video_id) for video_id in video_ids]
+
+
+def get_tmp_path(video_name) -> str:
+    return f"./.cache/{video_name}"
+
+
+def get_echo_voice_path(base_dir: str) -> str:
+    return f"{base_dir}/media/echo.wav"
 
 
 def download_youtube_video(url: str) -> None:
     video_id = get_video_id(url)
-    video_dir = get_video_dir(video_id)
-    if not os.path.exists(video_dir):
+    base_dir = get_base_dir(video_id)
+    if not os.path.exists(base_dir):
         command = ["yt-dlp", "-o", f"files/{video_id}/video.%(ext)s", url]
         subprocess.run(command)
-
+        metadata_path = get_metadata_path(base_dir)
         command = ["yt-dlp", "--get-title", url]
         title = subprocess.run(
             command, stdout=subprocess.PIPE, text=True
         ).stdout.strip()
-        with open(f"{video_dir}/meta.json", "w") as f:
+        with open(metadata_path, "w") as f:
             metadata = json.dumps({"title": title})
             f.write(metadata)
 
 
-def get_video_path(video_dir: str) -> str:
-    extensions = ["mp4", "avi", "mov", "webm"]
-    files = glob(f"{video_dir}/*.*")
-    return next((file for file in files if file.split(".")[-1] in extensions), None)
-
-
-def get_audio_path(video_dir: str) -> str:
-    return f"{video_dir}/audio.mp3"
-
-
-def get_audio_chunk_dir(video_dir: str) -> str:
-    return f"{video_dir}/chunks"
-
-
-def get_transcript_path(video_dir: str) -> str:
-    return f"{video_dir}/transcript.txt"
-
-
 def extract_audio_from_video(video_id: str) -> None:
-    video_dir = get_video_dir(video_id)
-    video_path = get_video_path(video_dir)
-    audio_path = get_audio_path(video_dir)
+    base_dir = get_base_dir(video_id)
+    video_path = get_video_path(base_dir)
+    audio_path = get_audio_path(base_dir)
     if not os.path.exists(audio_path):
         command = ["ffmpeg", "-i", video_path, "-vn", audio_path]
         subprocess.run(command)
 
 
-def cut_audio_in_chunks(video_id: str, chunk_size: int = 10) -> None:
-    video_dir = get_video_dir(video_id)
-    audio_path = get_audio_path(video_dir)
-    chunks_dir = get_audio_chunk_dir(video_dir)
-    os.makedirs(chunks_dir, exist_ok=True)
-    track = AudioSegment.from_mp3(audio_path)
+def cut_audio_in_chunks(source: str, destination: str, chunk_size: int = 10) -> None:
+    # os.makedirs(destination, exist_ok=True)
+    track = AudioSegment.from_mp3(source)
     chunk_len = chunk_size * 60 * 1000
     chunks = math.ceil(len(track) / chunk_len)
 
@@ -80,12 +116,11 @@ def cut_audio_in_chunks(video_id: str, chunk_size: int = 10) -> None:
         start_time = i * chunk_len
         end_time = (i + 1) * chunk_len
         chunk = track[start_time:end_time]
-        chunk.export(f"{chunks_dir}/chunk_{i}.mp3", format="mp3")
+        chunk.export(f"{destination}/chunk_{i}.mp3", format="mp3")
 
 
-def transcribe_chunks(video_dir: str):
-    chunks_dir = get_audio_chunk_dir(video_dir)
-    transcript_path = get_transcript_path(video_dir)
+def transcribe_chunks(base_dir: str, chunks_dir: str) -> None:
+    transcript_path = get_transcript_path(base_dir)
     if not os.path.exists(transcript_path):
         files = glob(f"{chunks_dir}/*")
         for file in tqdm(files, "Transcribing audio chunks"):
@@ -144,54 +179,20 @@ class VttOutputParser(BaseOutputParser):
         return " ".join(result)
 
 
-def get_metadata_path(video_id: str) -> str:
-    video_dir = get_video_dir(video_id)
-    return f"{video_dir}/meta.json"
-
-
-def get_video_name(video_id: str) -> str:
-    metadata_path = get_metadata_path(video_id)
-    with open(metadata_path, "r") as f:
-        result = json.loads(f.read())
-    return result["title"]
-
-
-def get_video_ids() -> list[str]:
-    return [video_path.lstrip("files/") for video_path in glob("files/*")]
-
-
-def get_video_name_map() -> dict:
-    video_ids = get_video_ids()
-    return {get_video_name(video_id): video_id for video_id in video_ids}
-
-
-def get_video_names() -> list[str]:
-    video_ids = get_video_ids()
-    return [get_video_name(video_id) for video_id in video_ids]
-
-
-def get_tmp_path(video_name):
-    return f"./.cache/{video_name}"
-
-
 def generate_video_id(length: int = 11) -> str:
     token = secrets.token_urlsafe(8)
     return token[:length]
 
 
-def move_permenent_dir(video_name: str, video_id: str) -> None:
+def move_to_permenent_dir(video_name: str, video_id: str) -> None:
     tmp_path = get_tmp_path(video_name)
-    video_dir = get_video_dir(video_id)
-    os.makedirs(video_dir, exist_ok=True)
+    base_dir = get_base_dir(video_id)
+    os.makedirs(base_dir, exist_ok=True)
     title, ext = video_name.split(".")
-    video_path = f"{video_dir}/video.{ext}"
+    video_path = f"{base_dir}/video.{ext}"
     if not os.path.exists(video_path):
         command = ["mv", tmp_path, video_path]
         subprocess.run(command)
-        with open(f"{video_dir}/meta.json", "w") as f:
+        with open(f"{base_dir}/meta.json", "w") as f:
             metadata = json.dumps({"title": title})
             f.write(metadata)
-
-
-def get_echo_voice_path(video_dir: str) -> str:
-    return f"{video_dir}/echo.wav"
